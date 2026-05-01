711383422a92ad500b90f41e02e20c5dee677674
Victor Gomes Diniz <dinizvictor2007@gmail.com>
2026-05-01 20:54:39 -0300

    .

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+function RolarDado(lados) {
+    var numero = Math.floor(Math.random() * lados ) +1;
+    if  (lados == 20)  {
+        document.getElementById("resultadod20").innerText = "Resultado: " + numero;
+    }
+    if (lados == 6) {
+        document.getElementById("resultadod6").innerText = "Resultado: " + numero;
+    }
+}   
+function pegarValor(){
+    let valor = document.getElementById("nome").value + "nigger";
+    document.getElementById("teste").innerText =  valor;
+}
